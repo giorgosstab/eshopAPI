6749f2b180e2f4044ff13e013600d33fb908f87a
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2018-09-22 21:50:17 +0300

    Relationship create Product/Reviews

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -5,5 +5,7 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model {
-
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
 }
